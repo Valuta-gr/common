d241ce9f978bbb9b2db4a575e322e9bcacb41a46
Edward Pasenidis <me@edpasenidis.tech>
2020-12-31 12:32:57 +0200

    feat: json validation && new error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-export * from './utils/env-is-provided';
+export * from "./utils/env-is-provided";
 
-export * from './errors/custom-error';
-export * from './errors/not-found-error';
+export * from "./errors/custom-error";
+export * from "./errors/not-found-error";
+export * from "./errors/request-validation-error";
 
-export * from './middlewares/error-handler';
+export * from "./middlewares/error-handler";
+export * from "./middlewares/validate-request";
