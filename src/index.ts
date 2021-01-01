adeb39f2973eb2258e2a743c2a0d6bf937a3104c
Edward Pasenidis <me@edpasenidis.tech>
2021-01-02 01:27:48 +0200

    refactor: remove useless imports

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,5 @@ export * from "./errors/bad-request-error";
 export * from "./errors/not-authorized-error";
 export * from "./errors/request-validation-error";
 
-export * from "./middlewares/require-auth";
-export * from "./middlewares/current-user";
 export * from "./middlewares/error-handler";
 export * from "./middlewares/validate-request";
