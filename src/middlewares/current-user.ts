ad86c212d23b8d4cc33da76a61b2f7c3f202329a
Edward Pasenidis <me@edpasenidis.tech>
2021-01-02 01:24:20 +0200

    wip: authentication

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -1,34 +1,10 @@
-import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
-
-interface UserPayload {
-  id: string;
+interface User {
+  id: string | number;
   email: string;
 }
 
-declare global {
-  namespace Express {
-    interface Request {
-      currentUser?: UserPayload;
-    }
+declare namespace Express {
+  export interface Request {
+    user?: User;
   }
 }
-
-export const currentUser = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  if (!req.session?.jwt) {
-    return next();
-  }
-  try {
-    const payload = jwt.verify(
-      req.session.jwt,
-      process.env.JWT_KEY!
-    ) as UserPayload;
-    req.currentUser = payload;
-  } catch (error) {}
-
-  next();
-};
